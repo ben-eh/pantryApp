27559491233109de90c183bff6fe89698a347d0e
osidebrandon <osidebrandon@gmail.com>
2023-06-11 14:39:21 -0600

    Created login screen

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Paper, TextField, Button, Divider } from '@mui/material';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Header = styled.header`
+  background-color: lightskyblue;
+  padding: 1rem 3rem;
+  display: flex;
+  align-items: center;
+`;
+const Body = styled.main`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 3rem;
+`;
+const Form = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+`;
+const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 1rem;
+`;
+
 
 function App() {
+
+  const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+        <Typography variant='h4'>
+          Pantry App
+        </Typography>
+      </Header>
+      <Body>
+        <Form elevation={5}>
+          <Title>
+            Login
+          </Title>
+          <TextField 
+            placeholder='Username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            style={{
+              marginBottom: '1rem',
+            }}
+          />
+          <TextField 
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{
+              marginBottom: '1rem',
+            }}
+          />
+          <Button
+            variant='contained'>
+            Login
+          </Button>
+        </Form>
+      </Body>
+    </Layout>
   );
 }
 
